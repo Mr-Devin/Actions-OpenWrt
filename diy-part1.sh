e571a29a2b9c5660a7c46d23cbc8d4c1f12fc81d
Devin Kw Zhu <34890117+Mr-Devin@users.noreply.github.com>
2024-09-17 22:23:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
-src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
+#src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+#src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git
