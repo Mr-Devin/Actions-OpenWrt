709f6cd92d65e9a6c07a1064899bd1d519574779
Devin Kw Zhu <34890117+Mr-Devin@users.noreply.github.com>
2024-09-17 22:49:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,19 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile feeds/luci/collections/luci-nginx/Makefile feeds/luci/collections/luci-light/Makefile
+sed -i 's/Bootstrap/argon/g' feeds/luci/collections/luci/Makefile feeds/luci/collections/luci-nginx/Makefile feeds/luci/collections/luci-light/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+
+sed -i sed -i "s/\.disabled='1'/\.disabled='0'/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+sed -i sed -i "s/\.ssid='OpenWrt'/\.ssid='2077'/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+
+# 默认密码
+sed -i "s/^root:::0:99999:7:::/root:\$1\$zQaIXyAb\$gyLqELl5w39SAgDAlg8A10:18376:0:99999:7:::/" package/base-files/files/etc/shadow
+
